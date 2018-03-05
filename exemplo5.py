418e3ad397e012f951c3649a975e7a10ca4f8970
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2018-03-05 09:55:46 -0300

    Add files via upload

---
+++ exemplo5.py
@@ -0,0 +1,46 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+"""Plotando e exportando em "csv" dados medios mensais de Tmax para 
+algumas localidades (medias do periodo 01/01/1980 a 31/07/2017)
+
+Arquivos:
+Tmax_daily_UT_Brazil_v2_19800101_19891231.nc
+Tmax_daily_UT_Brazil_v2_19900101_19991231.nc
+Tmax_daily_UT_Brazil_v2_20000101_20061231.nc
+Tmax_daily_UT_Brazil_v2_20070101_20131231.nc
+Tmax_daily_UT_Brazil_v2_20140101_20170731.nc
+"""
+
+# pegando variavel
+path_var = 'D:/Dropbox/ParaUbuntu/netcdfgrid3/'
+ds = xr.open_mfdataset(path_var + 'Tmax_daily_UT_Brazil_v2*.nc')
+var = ds['Tmax']
+
+# Nome dos pontos
+Names = ['INPE-SP', 'UFCG-PB', 'UFC-CE']
+lat_lon = [[-23.2, -45.9],
+         [-7.2, -35.9],
+         [-3.8, -38.6],]
+varMonthly2Export = pd.DataFrame(np.empty((12, len(Names))),
+                                 columns=Names,
+                                 index=range(1, 13))
+# media mensal
+for n, names in enumerate(Names):
+    tmaxCityDaily = var.sel(latitude=lat_lon[n][0],
+                            longitude=lat_lon[n][1],
+                            method='nearest')
+    tmaxCityMonthly = tmaxCityDaily.groupby('time.month').mean('time')
+    # ploting
+    tmaxCityMonthly.plot(label=names)
+    # concatenating in pandas
+    varMonthly2Export[names] = tmaxCityMonthly
+
+plt.title('')
+plt.legend()
+plt.show(block=False)
+
+# exporting
+varMonthly2Export.to_csv('dadosMensais.csv')
