d09ee1734d4ce8ba58394df32d1d1e2ff4f1a637
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2018-12-14 10:26:46 -0200

    Update abrindo_relevo.py

--- abrindo_relevo.py
+++ abrindo_relevo.py
@@ -2,7 +2,7 @@ import xarray as xr
 
 # Lendo dados GMTED2010 reamostrados para resolucao 0,1 grau
 path = r'D:\Dropbox\ParaUbuntu'
-alt = xr.open_mfdataset(path + '/GMTED2010_RES_GPM.nc')
+alt = xr.open_mfdataset(path + '/GMTED2010_RES_GPM_Community.nc')
 
 # para plotar
 alt.gmted2010_res_gpm.plot(cmap=plt.cm.terrain)
