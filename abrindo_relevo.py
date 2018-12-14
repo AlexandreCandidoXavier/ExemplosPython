41e14068b23acf31688aa00fd793049d26bc2565
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2018-12-14 10:12:42 -0200

    Update abrindo_relevo.py

--- abrindo_relevo.py
+++ abrindo_relevo.py
@@ -1,11 +1,11 @@
 import xarray as xr
 
-# Lendo dados GMTED2010 reamostrados para resolucao 0,1 graus
+# Lendo dados GMTED2010 reamostrados para resolucao 0,1 grau
 path = r'D:\Dropbox\ParaUbuntu'
 alt = xr.open_mfdataset(path + '/GMTED2010_RES_GPM.nc')
 
 # para plotar
-alt.gmted2010_res_gpm.plot()
+alt.gmted2010_res_gpm.plot(cmap=plt.cm.terrain)
 
 # altitude de determinada posicao
-alt.gmted2010_res_gpm.sel(lon=-50, lat=-15, method='nearest').values
+print(alt.gmted2010_res_gpm.sel(lon=-50, lat=-15, method='nearest').values)
