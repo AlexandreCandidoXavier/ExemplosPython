9bf741b02eb3e0a7d82d9daaba4ef5bd9943d770
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2020-02-19 10:33:06 -0300

    Update exemplo8.py

--- exemplo8.py
+++ exemplo8.py
@@ -1,7 +1,9 @@
 import xarray as xr
 import matplotlib.pyplot as plt
-"""
-Abrindo e plotando a normal da temperatura media do mês janeiro, período 1981-01-01-2009-12-31, 
+import cartopy.crs as ccrs
+from cartopy.feature import NaturalEarthFeature, BORDERS
+
+""" Abrindo e plotando a normal da temperatura media do mês janeiro, período 1981-01-01-2009-12-31, 
 utilizando as grade Tmax e Tmin, v2.1, resolução 0,1 graus. Arquivos necessários:
 Tmax_daily_UT_Brazil_v2.1_19800101_19891231.nc
 Tmax_daily_UT_Brazil_v2.1_19900101_19991231.nc
@@ -19,13 +21,23 @@ Tmin_daily_UT_Brazil_v2.1_20140101_20170731.nc
 day_first, day_last = '1981-01-01', '2009-12-31'
 
 # pegando Tmax e Tmin, v2.1 e calculando as msuas respectivas medias mensais
-path = 'D:/Dropbox/ParaUbuntu/netcdfgrid3/'
-tmax = xr.open_mfdataset(path + 'Tmax_daily_UT_Brazil_v2.1*.nc').Tmax
+path = '/home/alexandre/Dropbox/ParaUbuntu/netcdfgrid3/'
+tmax = xr.open_mfdataset(path + 'Tmax_daily_UT_Brazil_v2.1*.nc', combine='by_coords').Tmax
 tmax_month = tmax.sel(time=slice(day_first, day_last)).groupby('time.month').mean('time')
 
-tmin = xr.open_mfdataset(path + 'Tmin_daily_UT_Brazil_v2.1*.nc').Tmin
+tmin = xr.open_mfdataset(path + 'Tmin_daily_UT_Brazil_v2.1*.nc', combine='by_coords').Tmin
 tmin_month = tmin.sel(time=slice(day_first, day_last)).groupby('time.month').mean('time')
 
 # plotando normal, mes de Janeiro
-month = 1 # janeiro
+month = 1 # 1=janeiro, 2=fevereiro, .... 12=dezembro
 ((tmax_month.sel(month=month)+tmin_month.sel(month=month)) / 2).plot(cmap=plt.cm.jet)
+
+# plotando as medias para todos os meses
+t_media = ((tmax_month + tmin_month) / 2)
+p = t_media.plot(transform=ccrs.PlateCarree(), cmap=plt.cm.jet, col='month', col_wrap=4,
+                                    subplot_kws={'projection': ccrs.PlateCarree()}, extend='both')
+
+for ax in p.axes.flat:
+    ax.coastlines()
+    ax.add_feature(BORDERS)
+    ax.set_extent([-75, -33, -33.5, 6])
